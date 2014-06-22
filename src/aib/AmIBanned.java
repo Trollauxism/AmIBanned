4e7b09a62a3169f19516d3cb2871e4908fb72503
James Moreaux <trollaux@live.com>
2014-06-22 00:52:17 -0400

    Update AmIBanned.java
    In Changelog

--- src/aib/AmIBanned.java
+++ src/aib/AmIBanned.java
@@ -18,23 +18,23 @@ import javax.swing.GroupLayout.Alignment;
 
 
 public class AmIBanned extends JFrame {
-	
 	/**
-	 * 
+	 *  coded by iobsidian and trollaux
+	 * changelog:
+	 * optimized some shit and cleaned up code      - trollaux
+	 * initial build                                - iobsidian
 	 */
 	private static final long serialVersionUID = 1L;
-	static JPanel contentPane;
-	static JTextField txtEmail;
-	static JPasswordField passwordField;
-	static JTextPane txtpnWaiting = new JTextPane();
+	public static JPanel contentPane;
+	public static JTextField txtEmail;
+	public static JPasswordField passwordField;
+	public static JTextPane txtpnWaiting = new JTextPane();
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
 		EventQueue.invokeLater(new Runnable() {
-			
 			public void run() {
 				try {
 					AmIBanned frame = new AmIBanned();
@@ -45,9 +45,9 @@ public class AmIBanned extends JFrame {
 			}
 		});
 	}
-	
 	/**
 	 * Create the frame.
+	 * god damn this was fuckin hard to rewrite but i did it
 	 */
 	public AmIBanned() {
 		setOpacity(1);
@@ -57,71 +57,19 @@ public class AmIBanned extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
-		
 		passwordField = new JPasswordField();
-		
 		JLabel lblNewLabel = new JLabel("Email");
-		
 		JLabel lblPass = new JLabel("Password");
-		
 		JButton btnNewButton = new JButton("Am I Banned?");
-		
 		btnNewButton.addActionListener(new TestStatus());
-		
 		Box horizontalBox = Box.createHorizontalBox();
-		
-		
+
 		txtpnWaiting.setText("Waiting...");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(12)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel)
-								.addComponent(lblPass))
-							.addGap(4)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
-								.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE))
-							.addGap(12)
-							.addComponent(btnNewButton))
-						.addComponent(txtpnWaiting, GroupLayout.PREFERRED_SIZE, 377, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(180)
-							.addComponent(horizontalBox, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE))))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(13)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblNewLabel)
-							.addGap(11)
-							.addComponent(lblPass))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(7)
-							.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(13)
-							.addComponent(btnNewButton)))
-					.addGap(13)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(txtpnWaiting, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(22)
-							.addComponent(horizontalBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-		);
-		contentPane.setLayout(gl_contentPane);
-		
-		
+		// This is where it gets messy, but I didn't like having 2000000000000000 extra lines so DEAL WITH IT. Its pretty easy to figure out anyway.
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addGap(12).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblNewLabel).addComponent(lblPass)).addGap(4).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE).addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)).addGap(12).addComponent(btnNewButton)).addComponent(txtpnWaiting, GroupLayout.PREFERRED_SIZE, 377, GroupLayout.PREFERRED_SIZE).addGroup(gl_contentPane.createSequentialGroup().addGap(180).addComponent(horizontalBox, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)))));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addGap(13).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane.createSequentialGroup().addGap(3).addComponent(lblNewLabel).addGap(11).addComponent(lblPass)).addGroup(gl_contentPane.createSequentialGroup().addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE).addGap(7).addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)).addGroup(gl_contentPane.createSequentialGroup().addGap(13).addComponent(btnNewButton))).addGap(13).addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(txtpnWaiting, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE).addGroup(gl_contentPane.createSequentialGroup().addGap(22).addComponent(horizontalBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))));contentPane.setLayout(gl_contentPane);
 	}
 }
